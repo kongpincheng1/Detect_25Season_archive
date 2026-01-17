19beabe573c513bb93102545b1653c3b18113b7a
kpc <none>
2026-01-17 19:09:08 +0800

    这是25赛季仿真视觉代码的存档

--- setup.py
+++ setup.py
@@ -21,8 +21,7 @@ setup(
     entry_points={
         'console_scripts': [
             'detect = detect.detect_ros:main',
-            'test = detect.state_machine:main',  # 改为 state_machine 而非 test
-            'bucket_filter = detect.bucket_filter:main',  # 添加此行
+            'test = detect.detect_ros_sim_lowHZ:main',  
         ],
     },
 )
